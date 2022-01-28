0ad38d1de10c5e95f41f29bc6d7f9eeda16f4676
337-1 <senyuk@bsuir.by>
2022-01-28 12:25:04 +0300

    update WOrk

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.Locale;
 
@@ -9,23 +8,18 @@ public class Main {
     static final int YEAR = 1996;
     static final int MONTH = Calendar.AUGUST;
     static final int DAY = 15;
+
     public static void main(String[] args) {
 
         Calendar birthday = new GregorianCalendar(YEAR, MONTH, DAY);
         Calendar timeNow = GregorianCalendar.getInstance();
-        DateFormat sdf = new SimpleDateFormat(" - dd.MM.yyyy - E");
+        Locale locale = new Locale("be");
+        DateFormat sdf = new SimpleDateFormat(" - dd.MMMM.y - EEEE", locale);
         int i = 0;
         while (birthday.before(timeNow)) {
-            boolean check =  birthday.before(timeNow);
             System.out.println(i + sdf.format(birthday.getTime()));
             i++;
             birthday.set(Calendar.YEAR, YEAR + i);
         }
-
-       /*
-        Locale locale = new Locale("uk");
-
-        DateFormat df = new SimpleDateFormat("dd MMM yyy GG", locale);
-        System.out.println(df.format(cannes.getTime()));*/
     }
 }
